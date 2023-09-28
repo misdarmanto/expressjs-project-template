3234945574cb5414da7cb27c3c7f46f0bf92d301
misdarmanto <misdarmanto1922@gmail.com>
2023-09-28 10:32:17 +0700

    fix type

--- apps/controllers/index.ts
+++ apps/controllers/index.ts
@@ -1,18 +1,18 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
-import { ResponseData, ResponseDataAttributes } from "../utilities/response";
+import type { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { ResponseData } from '../utilities/response'
 
-export const index = async (req: Request, res: Response) => {
-	try {
-		const data = {
-			about_me: "Welcome to Crud Example API V1",
-		};
-		const response = <ResponseDataAttributes>ResponseData.default;
-		response.data = data;
-		return res.status(StatusCodes.OK).json(response);
-	} catch (error: any) {
-		const message = `unable to process request! error ${error.message}`;
-		const response = <ResponseDataAttributes>ResponseData.error(message);
-		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response);
-	}
-};
+export const index = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const data = {
+      about_me: 'Welcome to Crud Example API V1'
+    }
+    const response = ResponseData.default
+    response.data = data
+    return res.status(StatusCodes.OK).json(response)
+  } catch (error: any) {
+    const message = `unable to process request! error ${error.message}`
+    const response = ResponseData.error(message)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response)
+  }
+}
